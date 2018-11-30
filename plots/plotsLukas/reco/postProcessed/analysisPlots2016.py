0591ea8f0b5174aeeb6112e93a2fb7709fc1b0aa
Lukas Lechner <lukas.k.lechner@gmail.com>
2018-11-30 16:58:43 +0100

    bugfix SF/all plots & update for data plotting

--- plots/plotsLukas/reco/postProcessed/analysisPlots2016.py
+++ plots/plotsLukas/reco/postProcessed/analysisPlots2016.py
@@ -3,7 +3,7 @@
 '''
 
 # Standard imports
-import ROOT, os, imp
+import ROOT, os, imp, sys
 ROOT.gROOT.SetBatch(True)
 import itertools
 from math                             import isnan, ceil, pi
@@ -17,8 +17,8 @@ from TTGammaEFT.Tools.cutInterpreter  import cutInterpreter
 from TTGammaEFT.Tools.triggerSelector import triggerSelector
 
 from TTGammaEFT.Tools.objectSelection import getFilterCut
-from TTGammaEFT.Tools.objectSelection import nanoElectronVars, nanoMuonVars, nanoLeptonVars, nanoTauVars, nanoPhotonVars, nanoJetVars, nanoGenVars, nanoGenJetVars
-from TTGammaEFT.Tools.objectSelection import nanoElectronVarString, nanoMuonVarString, nanoLeptonVarString, nanoTauVarString, nanoPhotonVarString, nanoJetVarString, nanoGenVarString, nanoBJetVars, nanoBJetVarString, nanoGenJetVarString
+from TTGammaEFT.Tools.objectSelection import nanoDataElectronVars, nanoDataMuonVars, nanoDataLeptonVars, nanoDataTauVars, nanoDataPhotonVars, nanoDataJetVars, nanoDataBJetVars
+from TTGammaEFT.Tools.objectSelection import nanoDataElectronVarString, nanoDataMuonVarString, nanoDataLeptonVarString, nanoDataTauVarString, nanoDataPhotonVarString, nanoDataJetVarString, nanoDataBJetVarString
 
 # Default Parameter
 loggerChoices = ['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'TRACE', 'NOTSET']
@@ -27,8 +27,9 @@ loggerChoices = ['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'TRACE', 'NOTS
 import argparse
 argParser = argparse.ArgumentParser(description = "Argument parser")
 argParser.add_argument('--logLevel',           action='store',      default='INFO', nargs='?', choices=loggerChoices,                  help="Log level for logging")
-argParser.add_argument('--plot_directory',     action='store',      default='80X_TTG_v1')
-argParser.add_argument('--selection',          action='store',      default='dilepOS-nLepVeto2-pTG20-nPhoton1p-offZSF-mll40')
+argParser.add_argument('--plot_directory',     action='store',      default='80X_TTG_ppv3_v4')
+argParser.add_argument('--plotFile',           action='store',      default='allPlots')
+argParser.add_argument('--selection',          action='store',      default='dilepOS-nLepVeto2-pTG20-nPhoton1p-offZSFllg-offZSFll-mll40')
 argParser.add_argument('--small',              action='store_true',                                                                    help='Run only on a small subset of the data?', )
 argParser.add_argument('--noData',             action='store_true', default=False,                                                     help='also plot data?')
 argParser.add_argument('--signal',             action='store',      default=None,   nargs='?', choices=[None],                         help="Add signal to plot")
@@ -43,6 +44,13 @@ import RootTools.core.logger as logger_rt
 logger    = logger.get_logger(   args.logLevel, logFile = None)
 logger_rt = logger_rt.get_logger(args.logLevel, logFile = None)
 
+# Import plots list
+args.plotFile += '.py' if args.noData else '_unblinded.py'
+plotListFile   = os.path.join( os.path.dirname( os.path.realpath( __file__ ) ), 'plotLists', args.plotFile )
+if not os.path.isfile( plotListFile ):
+    logger.info( "Plot file not found: %s", plotListFile )
+    sys.exit(1)
+
 if args.small:           args.plot_directory += "_small"
 if args.noData:          args.plot_directory += "_noData"
 if args.signal:          args.plot_directory += "_signal_"+args.signal
@@ -51,10 +59,12 @@ if args.normalize:       args.plot_directory += "_normalize"
 if args.reweightPtZToSM: args.plot_directory += "_reweightPtZToSM"
 
 # 2016 Samples
-#data_directory = "/afs/hephy.at/data/llechner01/TTGammaEFT/nanoTuples/"
-postprocessing_directory = "TTGammaEFT_PP_2016_TTG_v1/inclusive/"
+#postprocessing_directory = "TTGammaEFT_PP_2016_TTG_v5/inclusive/"
+postprocessing_directory = "TTGammaEFT_PP_2016_TTG_v3/inclusive/"
 from TTGammaEFT.Samples.nanoTuples_Summer16_postProcessed    import *
-if not args.noData: from TTGammaEFT.Samples.nanoTuples_Run2016_05Feb2018_postProcessed import *
+if not args.noData:
+    postprocessing_directory = "TTGammaEFT_PP_2016_TTG_v5/inclusive/"
+    from TTGammaEFT.Samples.nanoTuples_Run2016_05Feb2018_postProcessed import *
 
 # Text on the plots
 def drawObjects( plotData, dataMCScale, lumi_scale ):
@@ -63,8 +73,8 @@ def drawObjects( plotData, dataMCScale, lumi_scale ):
     tex.SetTextSize(0.04)
     tex.SetTextAlign(11) # align right
     lines = [
-      (0.15, 0.95, 'CMS Preliminary' if plotData else 'CMS Simulation'), 
-      (0.45, 0.95, 'L=%3.1f fb{}^{-1} (13 TeV) Scale %3.2f'% ( lumi_scale, dataMCScale ) ) if plotData else (0.45, 0.95, 'L=%3.1f fb{}^{-1} (13 TeV)' % lumi_scale)
+      (0.15, 0.95, '#bf{CMS} #it{Preliminary}' if plotData else '#bf{CMS} #it{Simulation Preliminary}'), 
+      (0.45, 0.95, '%3.1f fb{}^{-1} (13 TeV) Scale %3.2f'% ( lumi_scale, dataMCScale ) ) if plotData else (0.65, 0.95, '%3.1f fb{}^{-1} (13 TeV)' % lumi_scale)
     ]
     return [tex.DrawLatex(*l) for l in lines] 
 
@@ -133,7 +143,6 @@ if args.reweightPtZToSM:
 # Sample definition
 if args.onlyTTG: mc = [ TTG_16 ]
 else:            mc = [ TTG_16, DY_LO_16, TT_pow_16, singleTop_16, ZGTo2LG ]
-#else:            mc = [ TTG_16, TT_pow_16]
 
 if args.noData:
     lumi_scale = 35.9
@@ -144,6 +153,7 @@ else:
     data_sample.name           = "data"
     data_sample.read_variables = [ "event/I", "run/I" ]
     data_sample.style          = styles.errorStyle( ROOT.kBlack )
+
     lumi_scale                 = data_sample.lumi * 0.001
     stack                      = Stack( mc, data_sample )
 
@@ -151,11 +161,13 @@ stack.extend( [ [s] for s in signals ] )
 
 for sample in stack.samples:
     sample.normalization = 1.
-    sample.read_variables = [] # add SF
+    if sample.name != "data":
+         sample.read_variables = ["isTTGamma/I", "isZGamma/I", "Photon[photonCat/I]"] # add SF
+    else:
+        sample.read_variables = [] # add SF
 
     if args.small:
-        nFiles = int( ceil( len( sample.files ) * 0.1 ) )
-        sample.reduceFiles( to = nFiles )
+        sample.reduceFiles( factor = 5 )
 
     sample.scale          = lumi_scale / sample.normalization
 #    sample.weight         = lambda event, sample: 1.
@@ -168,51 +180,58 @@ for sample in stack.samples:
 weight_ = lambda event, sample: event.weight
 tr = triggerSelector( 2016 )
 
-# Use some defaults (set defaults before you create list of Plots)
-Plot.setDefaults( stack=stack, weight=staticmethod( weight_ ), selectionString=cutInterpreter.cutString( args.selection ), addOverFlowBin='upper' )
-
 # Read variables and sequences
 read_variables  = ["weight/F", "ref_weight/F",
                    "nJet/I", "nAllJet/I", "nBTag/I",
-                   "Jet[%s]"         %nanoJetVarString,
+                   "Jet[%s]"         %nanoDataJetVarString,
                    "nLepton/I", "nLeptonTight/I", "nLeptonVeto/I", "nElectron/I", "nMuon/I",
-                   "Lepton[%s]"      %nanoLeptonVarString,
+                   "Lepton[%s]"      %nanoDataLeptonVarString,
                    "nPhoton/I",
-                   "Photon[%s]"      %(nanoPhotonVarString + ',photonCat/I'),
-                   "GenElectron[%s]" %nanoGenVarString,
-                   "GenMuon[%s]"     %nanoGenVarString,
-                   "GenPhoton[%s]"   %nanoGenVarString,
-                   "GenJet[%s]"      %nanoGenJetVarString,
-                   "GenBJet[%s]"     %nanoGenJetVarString,
-                   "GenTop[%s]"      %nanoGenVarString,
+                   "Photon[%s]"      %(nanoDataPhotonVarString),
+#                   "GenElectron[%s]" %nanoGenVarString,
+#                   "GenMuon[%s]"     %nanoGenVarString,
+#                   "GenPhoton[%s]"   %nanoGenVarString,
+#                   "GenJet[%s]"      %nanoGenJetVarString,
+#                   "GenBJet[%s]"     %nanoGenJetVarString,
+#                   "GenTop[%s]"      %nanoGenVarString,
                    "MET_pt/F", "MET_phi/F", "METSig/F", "ht/F",
                    "mll/F", "mllgamma/F",
                    "m3/F", "m3wBJet/F",
                    "lldR/F", "lldPhi/F", "bbdR/F", "bbdPhi/F",
                    "photonJetdR/F", "photonLepdR/F",
-                   "isTTGamma/I", "isZGamma/I",
+#                   "isTTGamma/I", "isZGamma/I",
+#                   "mL0Gamma/F",  "mL1Gamma/F",
+#                   "l0GammadR/F", "l0GammadPhi/F",
+#                   "l1GammadR/F", "l1GammadPhi/F",
+#                   "j0GammadR/F", "j0GammadPhi/F",
+#                   "j1GammadR/F", "j1GammadPhi/F",
                   ]
 
-read_variables += [ 'Bj0_' + var for var in nanoBJetVarString.split(',') ]
-read_variables += [ 'Bj1_' + var for var in nanoBJetVarString.split(',') ]
+
+read_variables += [ 'Bj0_' + var for var in nanoDataBJetVarString.split(',') ]
+read_variables += [ 'Bj1_' + var for var in nanoDataBJetVarString.split(',') ]
 
 # Sequence
 sequence = []
 
-# Import plots list (after setting Plot.setDefaults)
-plotListFile = os.path.join( os.path.dirname( os.path.realpath( __file__ ) ), 'plotLists', 'allPlots.py' )
-plotList     = imp.load_source( "plotList", os.path.expandvars( plotListFile ) ).plots
-
 # Loop over channels
 yields   = {}
 allPlots = {}
-#allModes = [ 'SF', 'all' ]
-allModes = [ 'mumu', 'mue', 'ee', 'all', 'SF' ]
-#allModes = [ 'mumu', 'mue', 'ee' ]
+allModes = [ 'mumu', 'mue', 'ee' ]
 
 for index, mode in enumerate( allModes ):
+    logger.info( "Computing plots for mode %s", mode )
+
     yields[mode] = {}
+
+    # Use some defaults (set defaults before you create list of Plots)
+    Plot.setDefaults( stack=stack, weight=staticmethod( weight_ ), selectionString=cutInterpreter.cutString( args.selection ) )#, addOverFlowBin='upper' )
+
+    # reload plot list module (necessary)
+    plotModule     = imp.load_source( "plotLists", os.path.expandvars( plotListFile ) )
+    from plotLists import plotList
     plots = plotList + [ getYieldPlot( index ) ]
+
     # Define 2l selections ( mumu, mue, ee, all )
     leptonSelection = cutInterpreter.cutString( mode )
 
@@ -233,41 +252,35 @@ for index, mode in enumerate( allModes ):
     for plot in plots:
         if plot.name != "yield": continue
         for i, l in enumerate( plot.histos ):
-          for j, h in enumerate( l ):
-            yields[mode][plot.stack[i][j].name] = h.GetBinContent( h.FindBin( 0.5+index ) )
-            h.GetXaxis().SetBinLabel( 1, "#mu#mu" )
-            h.GetXaxis().SetBinLabel( 2, "#mue" )
-            h.GetXaxis().SetBinLabel( 3, "ee" )
+            for j, h in enumerate( l ):
+                yields[mode][plot.stack[i][j].name] = h.GetBinContent( h.FindBin( 0.5+index ) )
+                h.GetXaxis().SetBinLabel( 1, "#mu#mu" )
+                h.GetXaxis().SetBinLabel( 2, "#mue" )
+                h.GetXaxis().SetBinLabel( 3, "ee" )
 
     if args.noData: yields[mode]["data"] = 0
 
     yields[mode]["MC"] = sum( yields[mode][s.name] for s in mc )
     dataMCScale        = yields[mode]["data"] / yields[mode]["MC"] if yields[mode]["MC"] != 0 else float('nan')
 
+    logger.info( "Plotting mode %s", mode )
     drawPlots( plots, mode, dataMCScale )
     allPlots[mode] = plots
 
-# Total data/MC scale
-#totalDataYield = sum( yields[m]["data"] for m in allModes )
-#totalMCYield   = sum( yields[m]["MC"]   for m in allModes )
-#dataMCScale = totalDataYield / totalMCYield if totalMCYield != 0 else float('nan')
+# Add the different channels into SF and all
+for mode in [ "SF", "all" ]:
+    yields[mode] = {}
 
+    for y in yields[allModes[0]]:
+        try:    yields[mode][y] = sum( yields[c][y] for c in ( ['ee','mumu'] if mode=="SF" else ['ee','mumu','mue'] ) )
+        except: yields[mode][y] = 0
 
-# Add plots of different channels into "all"
-#for mode, label in [ ('ee','SF'), ('mue','all') ]:
+    dataMCScale = yields[mode]["data"] / yields[mode]["MC"] if yields[mode]["MC"] != 0 else float('nan')
 
-#    print
+    for plot in allPlots['mumu']:
+        for pl in ( p for p in ( allPlots['ee'] if mode=="SF" else allPlots["mue"] ) if p.name == plot.name ):  #For SF add EE, second round add EMu for all
+            for i, j in enumerate( list( itertools.chain.from_iterable( plot.histos ) ) ):
+                j.Add( list( itertools.chain.from_iterable( pl.histos ) )[i] )
 
-#    for i_plot, plot in enumerate( allPlots['mumu'] ):                                                  # get all plots from first entry of allModes to add other modes
-#        print mode, label, plot.name, list( itertools.chain.from_iterable( plot.histos ) )                               # get all histos from there
-
-#        for i_hi, hi in enumerate( histoList0 ):                                                        # Loop over all histos to add the yields of other modes
-#        for i_hi, hi in enumerate( list( itertools.chain.from_iterable( plot.histos ) ) ):                                                        # Loop over all histos to add the yields of other modes
-
-#            specificPlotHistos = allPlots[mode][i_plot].histos                                          # get the histos from the specific mode which should be added
-#            specificHisto      = list( itertools.chain.from_iterable( specificPlotHistos ) )[i_hi]      # get the right histo of the specific mode to add to hi
-#            hi.Add( specificHisto )
-
-#    drawPlots( allPlots['mumu'], label, dataMCScale )
-
+    drawPlots( allPlots['mumu'], mode, dataMCScale )
 
--- plots/plotsLukas/reco/postProcessed/analysisPlots2016.sh
+++ plots/plotsLukas/reco/postProcessed/analysisPlots2016.sh
@@ -1,5 +1,24 @@
-nohup krenew -t -K 10 -- bash -c "python analysisPlots2016.py --small --noData --selection dilepOS-nLepVeto2-pTG20-nPhoton1p-offZSF-mll40" &
-nohup krenew -t -K 10 -- bash -c "python analysisPlots2016.py --small --noData --selection dilepOS-nLepVeto2-pTG20-nPhoton1p-onZSF-mll40" &
+small=""
+#small="--small"
+noData=""
+#noData="--noData"
 
-nohup krenew -t -K 10 -- bash -c "python analysisPlots2016.py --noData --selection dilepOS-nLepVeto2-pTG20-nPhoton1p-offZSF-mll40" &
-nohup krenew -t -K 10 -- bash -c "python analysisPlots2016.py --noData --selection dilepOS-nLepVeto2-pTG20-nPhoton1p-onZSF-mll40" &
+# Base line selection
+nohup krenew -t -K 10 -- bash -c "python analysisPlots2016.py ${small} ${noData} --selection dilepOS-nLepVeto2-pTG20-nPhoton1p-offZSFllg-offZSFll-mll40    --plot_directory 80X_TTG_ppv5_v1 --plotFile allPlots" &
+
+# Control region
+#nohup krenew -t -K 10 -- bash -c "python analysisPlots2016.py ${small} ${noData} --selection dilepOS-nLepVeto2-offZSFll-mll40                             --plot_directory 80X_TTG_ppv5_v1 --plotFile allPlots" &
+#nohup krenew -t -K 10 -- bash -c "python analysisPlots2016.py ${small} ${noData} --selection dilepOS-nLepVeto2-mll40                                      --plot_directory 80X_TTG_ppv5_v1 --plotFile mllPlots" &
+
+# Basic checks
+#nohup krenew -t -K 10 -- bash -c "python analysisPlots2016.py ${small} ${noData} --selection nJet2p-nBTag1p-nLepVeto2-mll20-dPhiJet0-dPhiJet1-offZSFll-OS --plot_directory 80X_TTG_ppv5_v1 --plotFile allPlots_StopsDilepton" &
+nohup krenew -t -K 10 -- bash -c "python analysisPlots2016.py ${small} ${noData} --selection nJet2p-nBTag1p-nLepVeto2-mll20-offZSFll-OS --plot_directory 80X_TTG_ppv5_v1 --plotFile allPlots_StopsDilepton" &
+
+# Signal regions
+#nohup krenew -t -K 10 -- bash -c "python analysisPlots2016.py ${small} ${noData} --selection dilepOS-nLepVeto2-pTG20-nPhoton1p-offZSFllg-offZSFll-mll40-nJet2p-nBTag0  --plot_directory 80X_TTG_ppv5_v1 --plotFile allPlots" &
+#nohup krenew -t -K 10 -- bash -c "python analysisPlots2016.py ${small} ${noData} --selection dilepOS-nLepVeto2-pTG20-nPhoton1p-offZSFllg-offZSFll-mll40-nJet2p-nBTag1  --plot_directory 80X_TTG_ppv5_v1 --plotFile allPlots" &
+#nohup krenew -t -K 10 -- bash -c "python analysisPlots2016.py ${small} ${noData} --selection dilepOS-nLepVeto2-pTG20-nPhoton1p-offZSFllg-offZSFll-mll40-nJet2p-nBTag2p --plot_directory 80X_TTG_ppv5_v1 --plotFile allPlots" &
+#nohup krenew -t -K 10 -- bash -c "python analysisPlots2016.py ${small} ${noData} --selection dilepOS-nLepVeto2-pTG20-nPhoton1p-offZSFllg-offZSFll-mll40-nJet1-nBTag1   --plot_directory 80X_TTG_ppv5_v1 --plotFile allPlots" &
+
+# For fun
+#nohup krenew -t -K 10 -- bash -c "python analysisPlots2016.py ${small} ${noData} --selection dilepOS-nLepVeto2-pTG20-nPhoton1p-offZSFllg-offZSFll-mll40-nJet2p-nBTag1p --plot_directory 80X_TTG_ppv5_v1 --plotFile allPlots" &
