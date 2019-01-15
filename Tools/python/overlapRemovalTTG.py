e33477f0d8a23b1a53773a1d84158c015ae638e5
Lukas Lechner <lukas.k.lechner@gmail.com>
2019-01-15 18:54:23 +0100

    update

--- Tools/python/overlapRemovalTTG.py
+++ Tools/python/overlapRemovalTTG.py
@@ -18,27 +18,33 @@ def getParentIds( g, genParticles ):
 
   if g['genPartIdxMother'] > 0:
     try:
-        mother1 = genParticles[g['genPartIdxMother']]
-        parents += [mother1['pdgId']] + getParentIds( mother1, genParticles )
+        mother1  = genParticles[ g['genPartIdxMother'] ]
+        parents += [ mother1['pdgId'] ] + getParentIds( mother1, genParticles )
     except:
         # when no 'status' selection is made for g, this can run in a kind of endless-loop, then python throws an Exception
         return parents
   return parents
 
 def hasMesonMother( parentList ):
-    # additionally check if the gluon is before the top, otherwise b > gluon q q with q > gamma q?
-    parentList = map( abs, set(parentList) )
-    return max(parentList) > 37 if len(parentList) != 0 else False
+    if not parentList: return False
+    return max( map( abs, parentList ) ) > 37
+
+def photonFromTopDecay( parentList ):
+    if not parentList:      return False  # empty list
+    parentList = map( abs, parentList )
+    if not 6 in parentList: return False  # ISR photon
+    if parentList[0] == 6:  return False  # photon from top
+    return True                           # photon from top decay
 
 def getPhotonCategory( g, genparts ):
 
     # safe time if g is no photon or electron
     if g is None or abs(g['pdgId']) not in [11,22]: return 3
 
-    isIsolated = isIsolatedPhoton( g, genparts, 0.2 )
+    isIsolated = isIsolatedPhoton( g, genparts, coneSize=0.2, ptCut=5, excludedPdgIds=[12,-12,14,-14,16,-16] )
     hasMeson   = hasMesonMother( getParentIds( g, genparts ) )
 
     if abs(g['pdgId']) == 22 and isIsolated and not hasMeson: return 0        # type 0: genuine photon:   isolated photon with no meson in parent list
     if abs(g['pdgId']) == 22 and isIsolated and hasMeson:     return 1        # type 1: hadronic photon:  isolated photon with meson in parent list
     if abs(g['pdgId']) == 11 and isIsolated and not hasMeson: return 2        # type 2: miss-Id electron: electron with deltaR and meson-mother requirement from genuine photon
-    return 3                                                                  # type 3: none of the above ( e.g. non-isolated )
+    return 3                                                                        # type 3: hadronic fake:    none of the above ( e.g. non-isolated )
