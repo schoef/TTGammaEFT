35d83664039a51492197a19a77cbbf958f9f3da2
Lukas Lechner <lukas.k.lechner@gmail.com>
2019-01-15 19:01:43 +0100

    trigger update

--- Tools/python/TriggerSelector.py
+++ Tools/python/TriggerSelector.py
@@ -1,54 +1,56 @@
 # Problems with HLT_DoubleEle33_CaloIdL_GsfTrkIdVL, HLT_TkMu50 and HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL
 
 class TriggerSelector:
-    def __init__( self, year, singleLepton=False ):
+    def __init__( self, year, era, singleLepton=False ):
 
+        self.singleLepton = singleLepton
+
+        # https://indico.cern.ch/event/718554/contributions/3027981/attachments/1667626/2674497/leptontriggerreview.pdf
         if year == 2016:
             if singleLepton:
                 self.m  = [ "HLT_IsoMu24", "HLT_IsoTkMu24" ]
-                self.e  = [ "HLT_Ele32_eta2p1_WPTight_Gsf" ]
-                self.mm = None
-                self.em = None
-                self.ee = None
-            else:
-                self.mm = [ "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL", "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ", "HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL", "HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ", "HLT_Mu30_TkMu11" ]
-#                self.m  = [ "HLT_IsoMu24", "HLT_IsoTkMu24", "HLT_Mu50", "HLT_TkMu50", "HLT_Mu45_eta2p1" ] #HLT_IsoTkMu24, HLT_IsoMu24
-                self.m  = [ "HLT_IsoMu24", "HLT_IsoTkMu24", "HLT_Mu50", "HLT_Mu45_eta2p1" ] #HLT_IsoTkMu24, HLT_IsoMu24
-#                self.ee = [ "HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ", "HLT_Ele17_Ele12_CaloIdL_TrackIdL_IsoVL_DZ", "HLT_DoubleEle33_CaloIdL_GsfTrkIdVL", "HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_MW" ]
-                self.ee = [ "HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ", "HLT_Ele17_Ele12_CaloIdL_TrackIdL_IsoVL_DZ", "HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_MW" ]
-                self.e  = [ "HLT_Ele27_WPTight_Gsf", "HLT_Ele105_CaloIdVT_GsfTrkIdT", "HLT_Ele115_CaloIdVT_GsfTrkIdT" ] #HLT_Ele105_CaloIdVT_GsfTrkIdT
-#                self.em = [ "HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL", "HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ", "HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL", "HLT_Mu17_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL", "HLT_Mu8_TrkIsoVVL_Ele17_CaloIdL_TrackIdL_IsoVL", "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL", "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ", "HLT_Mu30_Ele30_CaloIdL_GsfTrkIdVL" ]
-                self.em = [ "HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL", "HLT_Mu17_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL", "HLT_Mu8_TrkIsoVVL_Ele17_CaloIdL_TrackIdL_IsoVL"]
-
-        # not adapted yet
-        elif year == 2017:
-            if singleLepton:
-                self.m  = [ "HLT_IsoMu24" ]
                 self.e  = [ "HLT_Ele27_WPTight_Gsf" ]
                 self.mm = None
                 self.em = None
                 self.ee = None
             else:
-                self.mm = ["HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ", "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8"] # "HLT_Mu37_TkMu27" not in nanoAOD
-                self.m  = ["HLT_IsoMu27", "HLT_Mu50"]
-                self.ee = ["HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL", "HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ"]
-                self.e  = ["HLT_Ele35_WPTight_Gsf", "HLT_Ele32_WPTight_Gsf_L1DoubleEG"] # add single photon trigger? "HLT_Ele115_CaloIdVT_GsfTrkIdT" missing
-                self.em = ["HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ", "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ"]
+                self.mm = [ "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL", "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ" ]
+                self.m  = [ "HLT_IsoMu24", "HLT_IsoTkMu24" ]
+                self.ee = [ "HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ" ]
+                self.e  = [ "HLT_Ele27_WPTight_Gsf" ]
+                self.em = [ "HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL", "HLT_Mu17_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL", "HLT_Mu8_TrkIsoVVL_Ele17_CaloIdL_TrackIdL_IsoVL"]
+#                self.em = [ "HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL", "HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ", "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL", "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ"]
 
-        # not adapted yet
+        elif year == 2017:
+            if singleLepton:
+                self.m  = [ "HLT_IsoMu24", "HLT_IsoMu27" ]
+                self.e  = [ "HLT_Ele32_WPTight_Gsf_L1DoubleEG"]
+#                if era not in [ "B", "C" ]: self.e += [ "HLT_Ele32_WPTight_Gsf" ]
+                self.mm = None
+                self.em = None
+                self.ee = None
+            else:
+                self.mm = [ "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ" ]
+#                if era != "B": self.mm += [ "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8" ]
+                self.m  = [ "HLT_IsoMu24", "HLT_IsoMu27" ]
+                self.ee = [ "HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL" ]
+                self.e  = [ "HLT_Ele32_WPTight_Gsf_L1DoubleEG"]
+#                if era not in [ "B", "C" ]: self.e += [ "HLT_Ele32_WPTight_Gsf" ]
+                self.em = [ "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ", "HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ" ]
+
         elif year == 2018:
             if singleLepton:
                 self.m  = [ "HLT_IsoMu24" ]
-                self.e  = [ "HLT_Ele27_WPTight_Gsf" ]
+                self.e  = [ "HLT_Ele32_WPTight_Gsf" ]
                 self.mm = None
                 self.em = None
                 self.ee = None
             else:
-                self.mm = ["HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8", "HLT_Mu37_TkMu27"]
-                self.m  = ["HLT_IsoMu24", "HLT_IsoMu27", "HLT_Mu50"]
-                self.ee = ["HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL", "HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ"]
-                self.e  = ["HLT_Ele32_WPTight_Gsf", "HLT_Ele115_CaloIdVT_GsfTrkIdT", "HLT_Ele32_WPTight_Gsf_L1DoubleEG"]
-                self.em = ["HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ", "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ", "HLT_Mu27_Ele37_CaloIdL_MW", "HLT_Mu37_Ele27_CaloIdL_MW"]
+                self.mm = [ "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8" ]
+                self.m  = [ "HLT_IsoMu24" ]
+                self.ee = [ "HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL" ]
+                self.e  = [ "HLT_Ele32_WPTight_Gsf" ]
+                self.em = [ "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ", "HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ" ]
 
         else:
             raise NotImplementedError( "Trigger selection %r not implemented" %year )
@@ -71,7 +73,13 @@ class TriggerSelector:
         return "!%s" %cutString
 
     def getAllTrigger( self ):
-        return self.mm + self.m + self.ee + self.e + self.em
+        allTrigger = []
+        if self.mm: allTrigger += self.mm
+        if self.ee: allTrigger += self.ee
+        if self.em: allTrigger += self.em
+        if self.e:  allTrigger += self.e
+        if self.m:  allTrigger += self.m
+        return allTrigger
 
     def getSelection( self, PD ):
         found     = False
