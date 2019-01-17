1b9e89a16af1bcab0570890e39c15beebbd405a1
Lukas Lechner <lukas.k.lechner@gmail.com>
2019-01-17 11:05:55 +0100

    update proc cards

--- plots/plotsLukas/diagrams/runDiagrams.sh
+++ plots/plotsLukas/diagrams/runDiagrams.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-#declare -a cards=("TTGamma_SingleLeptFromT_3LineBuggy" "TTGamma_SingleLeptFromT_3LinePatched" "TTGamma_SingleLeptFromT_1Line" )
-declare -a cards=("TTGamma_SingleLeptFromT_3LineBuggy_full" "TTGamma_SingleLeptFromT_3LinePatched_full" "TTGamma_SingleLeptFromT_1Line_full" )
+#declare -a cards=("TTGamma_SingleLeptFromT_3LineBuggy_test" "TTGamma_SingleLeptFromT_3LinePatched_test" "TTGamma_SingleLeptFromT_1Line_test" )
+#declare -a cards=("TTGamma_SingleLeptFromT_3LineBuggy_full" "TTGamma_SingleLeptFromT_3LinePatched_full" "TTGamma_SingleLeptFromT_1Line_full" )
+#declare -a cards=("TTGamma_SingleLeptFromT_1Line_full" "TTGamma_SingleLeptFromT_1Line_test")
+#declare -a cards=("TTGamma_SingleLeptFromT_1Line")
+#declare -a cards=('TTGamma_DiLept_1Line_EFT' 'TTGamma_DiLept_1Line_EFT_test' 'TTGamma_Had_1Line_EFT' 'TTGamma_Had_1Line_EFT_test' 'TTGamma_SingleLeptFromT_1Line_EFT' 'TTGamma_SingleLeptFromT_1Line_EFT_test' 'TTGamma_SingleLeptFromTbar_1Line_EFT' 'TTGamma_SingleLeptFromTbar_1Line_EFT_test')
+#declare -a cards=('TTGamma_SingleLeptFromT_1Line' 'TTGamma_SingleLeptFromT_1Line_test' 'TTGamma_SingleLeptFromT_3LineBuggy' 'TTGamma_SingleLeptFromT_3LineBuggy_test' 'TTGamma_SingleLeptFromT_3LinePatched' 'TTGamma_SingleLeptFromT_3LinePatched_test')
+declare -a cards=('TTGamma_DiLept_1Line_EFT_test' 'TTGamma_Had_1Line_EFT_test' 'TTGamma_SingleLeptFromT_1Line_EFT_test' 'TTGamma_SingleLeptFromTbar_1Line_EFT_test')
+
+declare -a WC=("ctZ 2" "ctW 2" "ctG 2")
+#declare -a WC=()
 
 model="dim6top_LO"
 
@@ -10,7 +18,14 @@ model="dim6top_LO"
 for card in "${cards[@]}"
 do
 
-    nohup krenew -t -K 10 -- bash -c "getDiagrams.py --process ${card} --model ${model} --overwrite" > logs/${card}.log 2>&1 &
+#    nohup krenew -t -K 10 -- bash -c "getDiagrams.py --process ${card} --model ${model} --overwrite" &
+
+   for param in "${WC[@]}"
+   do
+
+    nohup krenew -t -K 10 -- bash -c "getDiagrams.py --process ${card} --model ${model} --couplings ${param} --overwrite" &
+
+   done
 
 done
 
