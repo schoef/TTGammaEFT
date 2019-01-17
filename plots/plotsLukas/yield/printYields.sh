4b14e6f9d2360deb76d3dd98906ee9185e59deee
Lukas Lechner <lukas.k.lechner@gmail.com>
2019-01-17 12:21:57 +0100

    bugfix weights in yields

--- plots/plotsLukas/yield/printYields.sh
+++ plots/plotsLukas/yield/printYields.sh
@@ -6,10 +6,8 @@ noData=""
 year="2016"
 
 # Base line selection
-nohup krenew -t -K 10 -- bash -c "python printYields.py ${small} ${noData} --year ${year} --selection dilepOS-nLepVeto2-pTG20-nPhoton1p-offZSFllg-offZSFll-mll40" &
-nohup krenew -t -K 10 -- bash -c "python printYields.py ${small} ${noData} --year ${year} --selection dilepOS-nLepVeto2-offZSFll-nJet2p-nBTag1p-mll20" &
-
-exit
+#nohup krenew -t -K 10 -- bash -c "python printYields.py ${small} ${noData} --year ${year} --selection dilepOS-nLepVeto2-pTG20-nPhoton1p-offZSFllg-offZSFll-mll40" &
+#nohup krenew -t -K 10 -- bash -c "python printYields.py ${small} ${noData} --year ${year} --selection dilepOS-nLepVeto2-offZSFll-nJet2p-nBTag1p-mll20" &
 
 nohup krenew -t -K 10 -- bash -c "python printYields.py ${small} ${noData} --year ${year} --selection dilepOS-nLepVeto2-pTG20-nPhoton1p-offZSFllg-offZSFll-mll40-nJet1-nBJet1" &
 nohup krenew -t -K 10 -- bash -c "python printYields.py ${small} ${noData} --year ${year} --selection dilepOS-nLepVeto2-pTG20-nPhoton1p-offZSFllg-offZSFll-mll40-nJet2p-nBJet0" &
@@ -20,6 +18,8 @@ nohup krenew -t -K 10 -- bash -c "python printYields.py ${small} ${noData} --yea
 nohup krenew -t -K 10 -- bash -c "python printYields.py ${small} ${noData} --year ${year} --selection dilepOS-nLepVeto2-pTG20-offZSFll-mll40-nJet1" &
 nohup krenew -t -K 10 -- bash -c "python printYields.py ${small} ${noData} --year ${year} --selection dilepOS-nLepVeto2-pTG20-offZSFll-mll40-nJet2" &
 nohup krenew -t -K 10 -- bash -c "python printYields.py ${small} ${noData} --year ${year} --selection dilepOS-nLepVeto2-pTG20-offZSFll-mll40-nJet3" &
+
+exit
 
 nohup krenew -t -K 10 -- bash -c "python printYields.py ${small} ${noData} --year ${year} --selection dilepOS-nLepVeto2-pTG20-offZSFll-mll40-nBJet0" &
 nohup krenew -t -K 10 -- bash -c "python printYields.py ${small} ${noData} --year ${year} --selection dilepOS-nLepVeto2-pTG20-offZSFll-mll40-nBJet1" &
